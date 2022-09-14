711a5ab0789bdd45f4f1decdd76e8bbd73758d25
Reem <reem.sharabi03@gmail.com>
2022-09-14 21:08:50 +0300

    Add files via upload

---
+++ Ch1/ArrBasedList/ArrBasedList.cpp
@@ -0,0 +1,48 @@
+#include "ArrBasedList.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+ArrBasedList::ArrBasedList()
+{
+	//a = new string[10];
+	s = 0; // the number of elements in the list
+}
+/*ArrBasedList::~ArrBasedList()
+{
+	for (int i=0; i<10; i++)
+		delete a[i];
+	delete [] a;
+}*/
+int ArrBasedList::size()
+{
+	return s;
+}
+string ArrBasedList::get(int i)
+{
+	return a[i];
+}
+void ArrBasedList::set(int i, string x)
+{
+	a[i] = x;
+}
+void ArrBasedList::add(int i, string x)
+{
+	if (s == 10)
+		cout << "You cannot add new elements." << endl;
+	else
+	{
+		for (int j = s; j > i; j--) // start from the right
+			a[j] = a[j - 1];
+	}
+	a[i] = x;
+	s++;
+}
+string ArrBasedList::remove(int i)
+{
+	string x = a[i];				// store the element
+	for (int j = i; j < s - 1; j++) // start from the right
+		a[j] = a[j + 1];
+	s--;
+	return x;
+}
