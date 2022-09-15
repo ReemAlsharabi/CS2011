93ddda403535300737054d82a788bc42efdf7f8a
Reem <reem.sharabi03@gmail.com>
2022-09-15 11:25:21 +0300

    Update Lec2Q3.py

--- Ch1/Lec2/Lec2Q3.py
+++ Ch1/Lec2/Lec2Q3.py
@@ -1,3 +1,12 @@
+'''
+Read the input one line at a time. At any point after reading the first 
+42 lines, if some line is blank (i.e., a string of length 0), then output the 
+line that occurred 42 lines prior to that one. For example, if Line 242 is 
+blank, then your program should output line 200. This program should 
+be implemented so that it never stores more than 43 lines of the input 
+at any given time
+'''
+
 f = open("words.txt", "r")
 queue = []
 words = f.readline()
