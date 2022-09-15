f9ba425e959dd055f3abc12252482cdeb0cccede
Reem <reem.sharabi03@gmail.com>
2022-09-15 11:28:28 +0300

    Update Lec3Q2.py

--- Ch1/Lec3/Lec3Q2.py
+++ Ch1/Lec3/Lec3Q2.py
@@ -1,3 +1,8 @@
+'''
+Exercise 1.4. Suppose you have a Stack, s, that supports only the push(x)
+and pop() operations. Show how, using only a FIFO Queue, q, you can
+reverse the order of all elements in s.
+'''
 n = int(input("How many elements do you have? "))
 s = []
 q = []
