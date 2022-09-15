854b18ba0ebdbb049ba4368f8d50d2856685e07b
Reem <reem.sharabi03@gmail.com>
2022-09-15 11:23:55 +0300

    Update Lec2Q1.py

--- Ch1/Lec2/Lec2Q1.py
+++ Ch1/Lec2/Lec2Q1.py
@@ -1,3 +1,9 @@
+'''
+Read the input one line at a time and then write the 
+lines out in reverse order, so that the last input line is 
+printed first, then the second last input line, and so 
+on.
+'''
 f = open("1-200.txt", "r")
 stack = []
 
