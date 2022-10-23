a59e0e38dd38f8ed190ff19af8c5e0d66ae150c1
Reem <reem.sharabi03@gmail.com>
2022-10-23 15:35:33 +0300

    Update Test.java

--- Sorting/Test.java
+++ Sorting/Test.java
@@ -3,7 +3,9 @@ public class Test
 	public static void main(String args[])
 	{
 		int arr[] = {1, 9, 3, 8, -1, 100, 0, 2, 8, 10};
-    
+		int l = 0;
+		int r = arr.length-1;
+		
 		System.out.println("selection sort");
 		//selection sort
 		SelectionSort selection = new SelectionSort();
@@ -21,14 +23,14 @@ public class Test
 		System.out.println("\n\nmerge sort");
 		//merge sort
 		MergeSort merge = new MergeSort();
-		merge.sort(arr);
+		merge.sort(arr, l, r);
 		for (int i=0; i<arr.length; i++)
 			System.out.print(arr[i] + " ");
 		
 		System.out.println("\n\nquick sort");
 		//quick sort
 		QuickSort quick = new QuickSort();
-		quick.sort(arr);
+		quick.sort(arr, l, r);
 		for (int i=0; i<arr.length; i++)
 			System.out.print(arr[i] + " ");
 				
