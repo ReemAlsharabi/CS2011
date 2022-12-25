df47457c14265ef864323c43ef74455ac3464353
Reem <reem.sharabi03@gmail.com>
2022-12-25 03:53:06 +0300

    Update MT.java

--- MT/MT.java
+++ MT/MT.java
@@ -9,37 +9,35 @@ public class MT
 {
 	public static void main(String[] args)
 	{
-		int a[] = {0, 1, 2, 3, 5, 6, 7, 8, 9, 10};
+		int a[] = {0, 1, 2, 4, 5, 6, 7, 8, 9};
 		System.out.println(findMissingInt(a));
 		System.out.println(findMissingInt2(a));
 	}	
 		
 	public static int findMissingInt(int []a) //O(n)
 	{
-		for (int i=0; i<a.length; i++)
+		for (int i=0; i<a.length-1; i++)
 			if (a[i]+1 != a[i+1] )
 				return a[i]+1;
 		return -1;
 	}
+	
 	
 	public static int findMissingInt2(int []a) //O(log n)
 	{
 		int low = 0;
 		int high = a.length-1;
 		int mid;
-		while (low <= high)
+		
+		while (high-low > 1)
 		{
 			mid = (low+high)/2;
-			if (a[mid]-1 == mid)
-				return a[mid]-1;
-			else if (mid <= a[mid]-1)
-				high = mid - 1;
-			else if (mid > a[mid]-1)
-				low = mid + 1;
+			if (a[low]-low != a[mid]-mid)
+				high = mid;
+			else if (a[high]-high != a[mid]-mid)
+				low = mid;
 		}
-		return -1;
+		return a[low]+1;
 	}
-	
-	//best case for both: O(1)
 }
 
